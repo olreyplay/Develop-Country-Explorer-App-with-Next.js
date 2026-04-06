56086fd4c4f1919af0a0942f15defdf5befbe052
Oleh Subotin <126164012+olreyplay@users.noreply.github.com>
2026-04-06 14:46:14 +0300

    Update CountriesList.tsx

--- components/CountriesList.tsx
+++ components/CountriesList.tsx
@@ -55,7 +55,7 @@ export default function CountriesList({ countries }: any) {
 
           <button
             onClick={handleReset}
-            className="rounded-md border border-gray-300  px-4 py-2 text-sm font-medium text-white transition hover:bg-gray-100"
+            className="rounded-md border border-gray-300  px-4 py-2 text-sm font-medium text-white transition "
           >
             Reset
           </button>
